b75fed7dd668a4333cb397c44cfdece23c4c67cd
nkazaryan <ghazaryan.nelly93@gmail.com>
2023-10-24 19:10:27 +0400

    this is my first python experience

---
+++ start.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def sum(x, y, z):
+    result = x + y + z
+    return result
+
+def prod(x, y):
+    result = x * y
+    return result
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a = "one "
+    b = "two "
+    c = "three"
+    s = sum(a, b, c)
+    ss = sum(7, 8, 9)
+    print(s)
+    print(ss)
+
+    x = 1
+    y = 2
+    z = prod(x, y)
+    print(z)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print('Hello world')
